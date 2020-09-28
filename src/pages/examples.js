53caa6f120b6c976292795e0ddcdebd79a6fa08f
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-09-28 16:14:47 +0300

    PageQuery

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -2,15 +2,39 @@ import React from 'react'
 import Layout from '../components/layout';
 import Header from '../examples/Header';
 import HeaderStatic from '../examples/HeaderStatic';
+import { graphql } from 'gatsby';
 
-const examples = () => {
+const examples = ({ data }) => {
+  // console.log(props)
+  const { site: {
+    info: { author }
+  }
+  } = data
+
   return (
     <Layout>
       <h1>hello from examples page</h1>
       <Header />
       <HeaderStatic />
+      <h5>author: {author}</h5>
     </Layout>
   )
 }
+
+export const data = graphql`
+query MyQuery {
+  site {
+    info:siteMetadata {
+      person {
+        name
+        age
+      }
+      title
+      description
+      author
+    }
+  }
+}
+`;
 
 export default examples

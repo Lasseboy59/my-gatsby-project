09ef2fdf9f3d257b841c4602905c201a693599a6
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-09-28 18:14:14 +0300

    added another path to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,15 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `src/images`,
+        name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
       },
     },
   ],

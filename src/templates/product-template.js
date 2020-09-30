70f3e36e0a5f8d735e20a82b832f6f1faecd2e22
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-09-30 10:37:37 +0300

    added gatsby-node for slug-info to templates

--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const producTemplate = () => {
+const producTemplate = (props) => {
+  console.log(props)
   return (
     <div>
       <h1>Hello from product template</h1>
